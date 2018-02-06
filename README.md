b3b62f51c8b5a06787eb07d716a4c7c59f09df0d
Anh Craft <huynhduyanh123123@gmail.com>
2018-02-06 21:37:21 +0700

    added placeholders feature

--- README.md
+++ README.md
@@ -20,6 +20,26 @@ The directory which you want to host packed folders
 
 Each directory separates by a comma
 
+### --placeholders
+
+Placeholders helps you to replace texts in output files
+
+Each placeholder separates by a vertical bar
+
+In each placeholder, there is a placeholder text and
+a directory of the file or a URL which you want to replace from, they 
+separates by a comma
+
+Example:
+
+> --placeholders "{text1},C:/texts/1.txt|{text2},C:/texts/2.txt"
+
+> --placeholders "%a%,C:/a.html"
+
+> --placeholders "{{content}},http://mywebsite.com/"
+
 ## Example
 
-java -Dfile.encoding=UTF-8 -Xmx1G -Xms1G -jar FilePacker.jar --input "C:/fd1,C:/fd2,C:/fd3,C:/fd4,C:/fd5" --output "C:/fd-packed"
+> java -Dfile.encoding=UTF-8 -Xmx1G -Xms1G -jar FilePacker.jar --input "C:/fd1,C:/fd2,C:/fd3,C:/fd4,C:/fd5" --output "C:/fd-packed"
+
+> java -Dfile.encoding=UTF-8 -Xmx1G -Xms1G -jar FilePacker.jar --placeholders "{text},https://google.com/|{a},C:/w.txt" "--input "C:/fd1,C:/fd2,C:/fd3,C:/fd4,C:/fd5" --output "C:/fd-packed"
--- src/org/anhcraft/filepacker/Main.java
+++ src/org/anhcraft/filepacker/Main.java
@@ -1,9 +1,9 @@
 package org.anhcraft.filepacker;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,16 +11,20 @@ import java.nio.file.attribute.DosFileAttributes;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Main {
     private static final String INPUT_ARG = "--input";
     private static final String OUTPUT_ARG = "--output";
+    private static final String PLACEHOLDER_ARG = "--placeholders";
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         System.out.println("FilePacker coded by anhcraft!");
         System.out.println("Initializing...");
         int arg_mode = -1;
+        LinkedHashMap<String, String> placeholders = new LinkedHashMap<>();
         LinkedHashMap<File, File> input = new LinkedHashMap<>();
         List<File> output = new ArrayList<>();
         for(String s : args){
@@ -29,7 +33,7 @@ public class Main {
                     File f = new File(fn);
                     if(f.exists()) {
                         if(f.isFile()) {
-                            input.put(f,f);
+                            input.put(f,f.getParentFile());
                         } else {
                             for(File sf : getFiles(f)){
                                 input.put(sf,f);
@@ -57,11 +61,36 @@ public class Main {
                 }
                 arg_mode = -1;
             }
+            else if(arg_mode == 2){
+                for(String p : s.split("\\|")){
+                    String[] x = p.split(",");
+                    String c = "";
+                    if(Pattern.compile("(https?|ftp):(/{1,})((?!-)([-a-zA-Z0-9]{1,})(?<!-))\\.((?!-)([-a-zA-Z0-9]{1,})).*")
+                            .matcher(x[1]).matches()){
+                        try {
+                            c = new Scanner(new URL(x[1]).openStream(), "UTF-8").useDelimiter("\\A").next();
+                        } catch(IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        try {
+                            c = new Scanner(new FileInputStream(new File(x[1])), "UTF-8").useDelimiter("\\A").next();
+                        } catch(FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    placeholders.put(x[0], c);
+                }
+                arg_mode = -1;
+            }
             else if(s.startsWith(INPUT_ARG)){
                 arg_mode = 0;
             }
             else if(s.startsWith(OUTPUT_ARG)){
                 arg_mode = 1;
+            }
+            else if(s.startsWith(PLACEHOLDER_ARG)){
+                arg_mode = 2;
             }
         }
         System.out.println("Starting...");
@@ -92,6 +121,13 @@ public class Main {
                     } catch(IOException e) {
                         e.printStackTrace();
                     }
+                    Path path = Paths.get(x.getAbsolutePath());
+                    Charset charset = StandardCharsets.UTF_8;
+                    String content = new String(Files.readAllBytes(path), charset);
+                    for(String r : placeholders.keySet()){
+                        content = content.replace(r, placeholders.get(r));
+                    }
+                    Files.write(path, content.getBytes(charset));
                     inc++;
                 }
             }
